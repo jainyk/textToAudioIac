0af0d48f0dc12adeb45ecafc6700f215a53a488b
jainyk <vjiit97@gmail.com>
2024-12-31 21:42:33 +0530

    code changs added

---
+++ terraform/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 5.25.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "mytexttoaudiobucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+  required_version = ">= 1.6.3"
+}
+
+# Testing agagin gaiin agagib
